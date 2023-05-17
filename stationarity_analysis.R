61e78337f8546aa709a44c7756922fd5d302408c
alexvmt <alexandermerdiantarko@gmail.com>
2023-05-18 00:07:45 +0200

    extend draft

--- stationarity_analysis.R
+++ stationarity_analysis.R
@@ -1,54 +1,77 @@
 ### stationarity analysis
 
-# disable scientific notations
+# disable scientific notation
 options(scipen = 999)
 
 # load packages
+library(readr)
 library(geosphere)
 library(ggplot2)
 
-# read raw data
-file <- "LifeTrack White Stork SW Germany SUBSET.csv"
-raw_data <- read.csv(file)
-dim(raw_data)
-
-# set relevant columns
-id_col <- "tag.local.identifier"
+# specify relevant columns (note that . is automatically replaced with - for column names in readr.read_csv())
+id_col <- "tag-local-identifier"
 time_col <- "timestamp"
-lon_col <- "location.long"
-lat_col <- "location.lat"
+lon_col <- "location-long"
+lat_col <- "location-lat"
 
-# subset raw data and rename columns
-preprocessed_data <- subset(raw_data, select = c(id_col, time_col, lon_col, lat_col))
-names(preprocessed_data) <- c("id", "time", "lon", "lat")
+# read data
+file <- "LifeTrack White Stork SW Germany.csv"
+data <- read_csv(paste0("../data/", file), col_select = c(id_col, time_col, lon_col, lat_col))
+dim(data)
 
-dim(preprocessed_data)
-length(unique(preprocessed_data$id))
+# rename columns
+names(data)[names(data) == id_col] <- "id"
+names(data)[names(data) == time_col] <- "time"
+names(data)[names(data) == lon_col] <- "lon"
+names(data)[names(data) == lat_col] <- "lat"
+
+# check dimensions and unique ids
+dim(data)
+length(unique(data$id))
 
 # get number of observations per year
-preprocessed_data$year <- substr(preprocessed_data$time, 1, 4)
-aggregate(cbind(count = year) ~ year, data = preprocessed_data, FUN = function(x){NROW(x)})
+data$year <- as.integer(format(data$time, format = "%Y"))
+aggregate(cbind(count = year) ~ year, data = data, FUN = function(x){NROW(x)})
 
 # filter data by year
-year <- "2022"
-preprocessed_data <- preprocessed_data[preprocessed_data$year == year,]
+year <- 2022
+data <- data[data$year == year, ]
+dim(data)
+length(unique(data$id))
+
+# get number of observations per id
+aggregate(cbind(count = id) ~ id, data = data, FUN = function(x){NROW(x)})
+
+# check for missing values
+colSums(is.na(data))
+
+# get number of missing values per id
+aggregate(cbind(count = id) ~ id, data = data[rowSums(is.na(data)) > 0, ], FUN = function(x){NROW(x)})
+
+# drop duplicated rows
+data <- data[!duplicated(data[c("id", "time")]), ]
+dim(data)
+length(unique(data$id))
+
+# order data
+data <- data[order(data$id, data$time), ]
 
 # create lag columns
-preprocessed_data$id_lag <- c(NA, head(preprocessed_data$id, -1))
-preprocessed_data$lon_lag <- c(NA, head(preprocessed_data$lon, -1))
-preprocessed_data$lat_lag <- c(NA, head(preprocessed_data$lat, -1))
+data$id_lag <- c(NA, head(data$id, -1))
+data$lon_lag <- c(NA, head(data$lon, -1))
+data$lat_lag <- c(NA, head(data$lat, -1))
 
-preprocessed_data$id_lag <- ifelse(preprocessed_data$id == preprocessed_data$id_lag,
-                                   preprocessed_data$id_lag,
-                                   NA)
+data$id_lag <- ifelse(data$id == data$id_lag,
+                      data$id_lag,
+                      NA)
 
-preprocessed_data$lon_lag <- ifelse(preprocessed_data$id == preprocessed_data$id_lag,
-                                    preprocessed_data$lon_lag,
-                                    NA)
+data$lon_lag <- ifelse(data$id == data$id_lag,
+                       data$lon_lag,
+                       NA)
 
-preprocessed_data$lat_lag <- ifelse(preprocessed_data$id == preprocessed_data$id_lag,
-                                    preprocessed_data$lat_lag,
-                                    NA)
+data$lat_lag <- ifelse(data$id == data$id_lag,
+                       data$lat_lag,
+                       NA)
 
 # calculate distance between two measurements
 calculate_distance_in_meters_between_coordinates <- function(lon_a, lat_a, lon_b, lat_b) {
@@ -56,35 +79,42 @@ calculate_distance_in_meters_between_coordinates <- function(lon_a, lat_a, lon_b
   distm(c(lon_a, lat_a), c(lon_b, lat_b), fun = distHaversine)
   }
 
-preprocessed_data$distance_meters <- mapply(lon_a = preprocessed_data$lon,
-                                            lat_a = preprocessed_data$lat,
-                                            lon_b = preprocessed_data$lon_lag,
-                                            lat_b = preprocessed_data$lat_lag,
-                                            FUN = calculate_distance_in_meters_between_coordinates)
+data$distance_meters <- mapply(lon_a = data$lon,
+                               lat_a = data$lat,
+                               lon_b = data$lon_lag,
+                               lat_b = data$lat_lag,
+                               FUN = calculate_distance_in_meters_between_coordinates)
+
+# extract hour and day from time column
+data$day_hour <- droplevels(cut(data$time, breaks = "hour"))
+data$day <- droplevels(cut(data$time, breaks = "day"))
 
 # aggregate distances by time interval
-preprocessed_data$time <- as.POSIXct(preprocessed_data$time, format="%Y-%m-%d %H:%M:%S", tz="UTC")
-
-preprocessed_data$day_hour <- droplevels(cut(preprocessed_data$time, breaks = "hour"))
-preprocessed_data$day <- droplevels(cut(preprocessed_data$time, breaks = "day"))
-
-data_agg_id_day_hour <- aggregate(distance_meters ~ day_hour + id, data = preprocessed_data, FUN = sum)
-data_agg_id_day <- aggregate(distance_meters ~ day + id, data = preprocessed_data, FUN = sum)
+data_agg_id_day_hour <- aggregate(distance_meters ~ day_hour + id, data = data, FUN = sum)
+data_agg_id_day <- aggregate(distance_meters ~ day + id, data = data, FUN = sum)
 
 length(unique(data_agg_id_day_hour$id))
 length(unique(data_agg_id_day$id))
 
 # plot aggregated distances by time interval
-unique(data_agg_id_day$id)
-id <- 3264
+data_agg_id_day$day <- as.Date(data_agg_id_day$day)
+ggplot(data_agg_id_day, aes(x = day, y = distance_meters, group = 1)) +
+  geom_line(size = 0.75) +
+  facet_wrap(id~., switch = "y", ncol = 1) +
+  scale_x_date(breaks = seq(as.Date("2022-01-01"), as.Date("2022-12-31"), by = "1 month"), date_labels = "%b") +
+  ggtitle(paste0("Distance in meters moved per day for year = ", year))
 
-ggplot(data_agg_id_day[data_agg_id_day$id == id,], aes(day, distance_meters, group = 1)) +
-  theme_bw() +
-  geom_line() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  ggtitle(paste0("Distance in meters moved per day for id ", id, " and year ", year, "\n(location measurement every 5 minutes)"))
-  
+# check out individual ids
+id <- 3921
+ggplot(data_agg_id_day[data_agg_id_day$id == id, ], aes(x = day, y = distance_meters, group = 1)) +
+  geom_line(size = 0.75) +
+  scale_x_date(breaks = seq(as.Date(paste0(year, "-01-01")), as.Date(paste0(year, "-12-31")), by = "1 month"), date_labels = "%b") +
+  ggtitle(paste0("Distance in meters moved per day for id = ", id, " and year = ", year))
 
 
+
+# take last n months per individual and calculate total and mean distance moved per set time interval
+# compare distance in last n-m months to last n months
+# movement in last time interval (e. g. 1 day) compared to average movement in last time interval * m but without outliers (95%)
 # identify stationary tags
 # plot stationary tags on map
